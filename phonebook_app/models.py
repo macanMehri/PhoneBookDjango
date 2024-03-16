52365fe0d46238afedb8ebee24be90797f739a5a
Macan Mehri <macanmehri@gmail.com>
2024-03-16 17:54:17 +0330

    Created a base class in models file

--- phonebook_app/models.py
+++ phonebook_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    is_active = models.BooleanField(
+        default=False,
+        verbose_name='Is Active'
+    )
+
+    created_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Created Date'
+    )
+
+    updated_date = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Updated Date'
+    )
+
+    class Meta:
+        abstract = True
+    
+
+    def __str__(self) -> str:
+        raise NotImplementedError('You did not override the string method!')
