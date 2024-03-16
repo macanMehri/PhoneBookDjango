1b1a444fba4317017f47100624dc243f13506026
Macan Mehri <macanmehri@gmail.com>
2024-03-16 18:10:54 +0330

    Created two models. An Address model and a Person model

--- phonebook_app/models.py
+++ phonebook_app/models.py
@@ -23,3 +23,71 @@ class BaseModel(models.Model):
 
     def __str__(self) -> str:
         raise NotImplementedError('You did not override the string method!')
+
+
+class Address(BaseModel):
+    country = models.CharField(
+        max_length=255,
+        null=False,
+        verbose_name='Country'
+    )
+
+    city = models.CharField(
+        max_length=255,
+        null=False,
+        verbose_name='City'
+    )
+
+    exact_location = models.TextField(
+        verbose_name='Exact location'
+    )
+
+    class Meta:
+        verbose_name = 'Address'
+        verbose_name_plural = 'Addresses'
+    
+
+    def __str__(self) -> str:
+        return f'{self.city} in {self.country} - {self.exact_location}'
+
+
+class Person(BaseModel):
+
+    first_name = models.CharField(
+        max_length=255,
+        null=False,
+        verbose_name='First Name'
+    )
+    
+    last_name = models.CharField(
+        max_length=255,
+        null=False,
+        verbose_name='Last Name'
+    )
+
+    number = models.CharField(
+        max_length=12,
+        null=False,
+        verbose_name='Phone Number'
+    )
+
+    address = models.ForeignKey(
+        Address,
+        on_delete=models.deletion.PROTECT,
+        null=False,
+        verbose_name='Address'
+    )
+
+    
+    class Meta:
+        verbose_name = 'Person'
+        verbose_name_plural = 'People'
+
+    
+    @property
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+    def __str__(self) -> str:
+        return f'{self.full_name} - {self.number} - {self.address}'
