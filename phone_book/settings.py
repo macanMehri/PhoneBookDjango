c8e9f5348b43adfcc97b6f35699868fd34c1fde6
Macan Mehri <macanmehri@gmail.com>
2024-03-16 17:41:00 +0330

    Removed security informations from settings and moved to local_settings file

--- phone_book/settings.py
+++ phone_book/settings.py
@@ -19,14 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@d515_qd_l-p30goo&g)az&k13_)ts25_2an=ww@wpl!!%6=k_'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
 
 # Application definition
 
