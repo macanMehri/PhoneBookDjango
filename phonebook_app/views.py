b1109606fa9868f0aeca04a3ea1a8ae0bd12ce04
Macan Mehri <macanmehri@gmail.com>
2024-03-16 19:27:14 +0330

    Created two api views for person and address models

--- phonebook_app/views.py
+++ phonebook_app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets, filters, permissions
+from .serializers import AddressSerializer, PersonSerializer
+from .models import Person, Address
 
-# Create your views here.
+
+class AddressViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = AddressSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = Address.objects.filter(is_active=True,).order_by('pk')
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter)
+
+
+class PersonViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = PersonSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = Person.objects.filter(is_active=True,).order_by('pk')
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter)
